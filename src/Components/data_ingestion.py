f034999697570dc513c0f18cfab8fdf8d746e30e
Chirag Mahajan <mahajan.chirag3@gmail.com>
2023-10-30 19:56:24 -0400

    Data Ingestion

---
+++ src/Components/data_ingestion.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from src.exception import CustomException
+from src.logger import logging
+from dataclasses import dataclass
+
+@dataclass
+class dataingestion_config:
+    train_data_path: str=os.path.join('artifacts','train.csv')
+    test_data_path: str=os.path.join('artifacts','test.csv')
+    raw_data_path: str=os.path.join('artifacts','raw.csv')
+
+
+class dataingestion:
+    def __init__(self):
+        self.ingestion_config=dataingestion_config()
+    
+    def initiate_ingest(self):
+        try:
+            df=pd.read_csv('Notebook\Data\churn.csv')
+            logging.info('Data has been read')
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info('Created path')
+            train_set,test_set=train_test_split(df,test_size=0.25,random_state=1)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('Train test splitting done')
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=='__main__':
+    obj=dataingestion()
+    obj.initiate_ingest()
--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd,'logs',log_file)
+logs_path=os.path.join(os.getcwd(),'logs',log_file)
 os.makedirs(logs_path,exist_ok=True)
 LOG_FILE_PATH=os.path.join(logs_path,log_file)
 logging.basicConfig(
